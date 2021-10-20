8b8f02a70340ed9f82643885df1a656483c1206d
Bagas Tri Genindaru <45539721+bagastri@users.noreply.github.com>
2021-10-20 14:28:49 +0700

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,9 @@
+kata_sifat = input("Sebutkan sebuah Kata Sifat: ")
+kegiatan = input("Sebutkan sebuah Kegiatan: ")
+nama_orang = input("Sebutkan Nama Seseorang: ")
+tempat = input("Sebutkan sebuah Nama Tempat: ")
+
+print(f"Aku adalah Seorang yang {kata_sifat}")
+print(f"Hobiku adalah {kegiatan}")
+print(f"Aku Suka dengan {nama_orang}")
+print(f"Aku Punya Kebiasaan Tidur di {tempat}")
